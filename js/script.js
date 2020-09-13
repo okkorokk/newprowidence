8b4adda57b2423fc55d101a7f2faa8c6e4ca11f7
okkorokk <69345811+okkorokk@users.noreply.github.com>
2020-09-13 21:53:36 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let burger = document.getElementsByClassName("burger-menu");
 let body = document.getElementById("body");
 let menu = document.getElementById("menu");
+let slider_track = document.querySelector(".slider-track");
 for (let i = 0; i < burger.length; i++) {
 	burger[i].onclick = function () {
 		if (!menu.classList.contains("mobile-menu")) {
+			slider_track.style.display = "none";
 			burger[i].classList.add("active");
 			menu.classList.add("mobile-menu");
 			body.classList.add("lock");
@@ -11,6 +13,7 @@ for (let i = 0; i < burger.length; i++) {
 			burger[i].classList.remove("active");
 			menu.classList.remove("mobile-menu");
 			body.classList.remove("lock");
+			slider_track.style.display = "flex";
 		}
 	};
 }
